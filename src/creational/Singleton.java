94ae624302f09a549201ae40b2c1edd081adc5e5
hubert <hubertdang@cmail.carleton.ca>
2025-03-31 12:17:23 -0400

    Implement template for Singleton pattern

---
+++ src/creational/Singleton.java
@@ -0,0 +1,40 @@
+package creational;
+
+import java.security.SignedObject;
+
+/**
+ * Represents a singleton - a class that can have at most one instance at
+ * any given time, accessible to clients from a well-known access point.
+ */
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {}
+
+    /**
+     * Provides access to the instance of the singleton. If the instance
+     * has not already been instantiated, instanstiates it first.
+     * 
+     * @return The sole instance of the singleton.
+     */
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+
+        return instance;
+    }
+
+    /**
+     * Example usage.
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        Singleton singletonInstance = Singleton.getInstance();
+        
+        if (Singleton.getInstance() == singletonInstance) {
+            System.out.println("Singleton.getInstance() should return the same instance.");
+        }
+    }
+}
